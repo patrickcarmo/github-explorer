c284e7f2b4fec0e89bd0c0769650a9ef5191a68e
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2020-09-19 19:29:59 +0100

    Updates

---
+++ precache-manifest.0cb99a7024fd6c0d7ce4ded8f1b09640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30577f8ee8186789c0d58b5f8aa2f1ac",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "6eb04949779d43b385dc",
+    "url": "/github-explorer/static/js/2.44b8a4c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.44b8a4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12fa2cb0e55db3ac478b",
+    "url": "/github-explorer/static/js/main.7acdc58d.chunk.js"
+  },
+  {
+    "revision": "b76f610d1055037be066",
+    "url": "/github-explorer/static/js/runtime-main.efd11135.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  }
+]);
