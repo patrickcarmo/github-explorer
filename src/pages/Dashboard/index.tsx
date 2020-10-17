6d39965edb151024ff5cddc98ae5b429c925954b
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2020-10-17 13:52:10 +0100

    added dashboard content and style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Form, Repositories, Title } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositories on Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositories on Github</Title>
+
+      <Form>
+        <input placeholder="Type the repository name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars3.githubusercontent.com/u/29663302?s=400&u=cde757625d1616cb3474e6f07aa8156565e45408&v=4"
+            alt="Patrick Carmo"
+          ></img>
+          <div>
+            <strong>patrickcarmo/github-expolorer</strong>
+            <p>Using React to consume Api from Github.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,104 @@
+import { shade } from 'polished';
 import styled from 'styled-components';
+
+const colorGray = '#3a3a3a';
+const marginTop = '80px';
+const maxWidth = '700px';
+const formHeight = '70px';
+const colorGreen = '#04d361';
+const colorWhite = '#fff';
 
 export const Title = styled.h1`
   font-size: 48px;
-  color: #3a3a3a;
+  color: ${colorGray};
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: ${marginTop};
+`;
+
+export const Form = styled.form`
+  margin-top: calc(${marginTop} / 2);
+  max-width: ${maxWidth};
+  display: flex;
+
+  input {
+    color: ${colorGray};
+    flex: 1;
+    height: ${formHeight};
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: ${formHeight};
+    background: ${colorGreen};
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: ${colorWhite};
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, colorGreen)};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: calc(${marginTop} / 2);
+  max-width: ${maxWidth};
+
+  a {
+    background: ${colorWhite};
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(16px);
+      transition: transform 0.2s;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
